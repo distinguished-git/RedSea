d98a84a3d677a2be12225a263c135e865bdc9128
distinguished-git <hb84@law.georgetown.edu>
2020-12-04 01:01:07 -0500

    postproc

---
+++ postproc-flac.sh
@@ -0,0 +1,42 @@
+DIRNAME="$1"
+
+cd "$DIRNAME"
+
+touch ./valid.log
+
+
+butler() {
+	for flac in *.flac
+	do
+		
+		echo "$flac"
+		if grep -Fxq "$flac" ./valid.log
+		then 
+			echo "already checked"
+			exit 0
+		fi
+
+		# if there's a problem with the flac file
+		if flac --test --warnings-as-errors --silent "$flac" ; 
+		then
+			echo "$flac" >> ./valid.log
+
+			TXTFILE="$(echo "$flac" | sed "s/\.flac/\.txt/")"
+			if [ -e $TXTFILE ]
+			then
+			    metaflac --import-tags-from "$TXTFILE" "$flac"
+			    rm -f $TXTFILE
+			fi
+		else
+			rm -f "$flac"
+		fi
+	done
+}
+
+butler
+
+python3 /tmp/ramdisk/RedSea/redsea.py --preset default --account tv4 --skip "https://tidal.com/browse/album/$(jq '.id' album.json)"
+
+butler
+
+flac --replay-gain --keep-foreign-metadata *.flac
