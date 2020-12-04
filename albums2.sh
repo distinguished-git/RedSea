6d62030b3d4641d162361bc563023bd3a840b6ff
distinguished-git <hb84@law.georgetown.edu>
2020-12-04 09:47:20 -0500

    .

---
+++ albums2.sh
@@ -0,0 +1,25 @@
+cat albums2.txt | while read line 
+do
+	python3 /tmp/ramdisk/RedSea/redsea.py --preset default --skip --account tv5 "$1"
+	find "/tmp/ramdisk/tidal" -iname "album.json" -exec "/tmp/ramdisk/RedSea/postproc-flac.sh" "{}" \;
+done
+
+
+
+
+exit
+DIR="$(pwd)"
+screen -dmS dnf sudo dnf install -y jq flac
+if ! bpm-tag -h
+then
+	mkdir /tmp/install-bpm-tools
+	cd /tmp/install-bpm-tools
+	wget https://www.pogo.org.uk/~mark/bpm-tools/releases/bpm-tools-0.3.tar.gz
+	tar -zxvf bpm-tools-0.3.tar.gz
+	cd bpm-tools-0.3
+	make
+	sudo make install
+	cd "$DIR"
+	rm -rf /tmp/install-bpm-tools
+fi
+
